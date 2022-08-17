78997cdcf456fde0e477dcdfbca8643d77c30c76
Akshayw26 <wangade30@gmail.com>
2022-08-17 16:00:54 +0530

    Added -Used Oops Concept

--- src/com/LineComparison/LineComparison.java
+++ src/com/LineComparison/LineComparison.java
@@ -3,46 +3,39 @@ package com.LineComparison;
 import java.util.Scanner;
 
 public class LineComparison {
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to line comparison computation program");
-        System.out.println("----------------------------------------------");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter x1 : ");
-        double x1 = scanner.nextDouble();
-        System.out.println("Enter x2 : ");
-        double x2 = scanner.nextDouble();
-        System.out.println("Enter y1 : ");
-        double y1 = scanner.nextDouble();
-        System.out.println("Enter y2 : ");
-        double y2 = scanner.nextDouble();
-
-        System.out.println("Enter x3 : ");
-        double x3=scanner.nextDouble();
-        System.out.println("Enter x4 : ");
-        double x4=scanner.nextDouble();
-        System.out.println("Enter y3 : ");
-        double y3=scanner.nextDouble();
-        System.out.println("Enter y4 : ");
-        double y4=scanner.nextDouble();
-
+    float x1, x2, x3, x4, y1, y2, y3, y4;
+    LineComparison(float x1, float x2, float x3, float x4, float y1, float y2, float y3, float y4){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.y3 = y3;
+        this.y4 = y4;
+    }
+    void lengths() {
         Double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         Double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
-
-        System.out.println("-------------------------------------");
-        System.out.println(" Length of line 1 is  : "+lineLength1);
-        System.out.println(" Length of line 2 is  : "+lineLength2);
-        //used compareTo method
-        if(compare(lineLength1,lineLength2)) {
-            System.out.println(" Line 1 is greater than line 2 ");
+        if (lineLength1.equals(lineLength2)) {
+            System.out.println("Both lines are equal");
+        }else if (compare(lineLength1, lineLength2)) {
+            System.out.println("Length 1 is greater than Length 2");
         }else {
-            System.out.println(" line 2 is greater than line 1 ");
+            System.out.println("Length 2 is greater than Length 1");
         }
     }
-          private static boolean compare(double lineLength1, double lineLength2) {
-           return false;
 
-        }
+    private boolean compare(Double lineLength1, Double lineLength2) {
+        return false;
     }
+    public static void main(String[] args) {
+
+        LineComparison lc = new LineComparison(4,6,7,2,3,4,7,1);
+
+        lc.lengths();
+
+    }
+}
 
 
